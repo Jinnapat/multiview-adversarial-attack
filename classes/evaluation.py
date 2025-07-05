94fa0ec236bca9e855feb9121d86609c736ccad5
jinnapatyana <jaja_yana@hotmail.com>
2025-07-05 20:29:48 +0800

    update evaluate function

--- classes/evaluation.py
+++ classes/evaluation.py
@@ -19,8 +19,8 @@ def evaluate(model, preprocess, dataloader, gt_class):
             top5_classes = topk(preds, k=5).indices
 
             top1 += (top5_classes[:, 0] == gt_class).sum().item()
-            top3 += top1 + (top5_classes[:, 1:3] == gt_class).any(dim=1).sum().item()
-            top5 += top3 + (top5_classes[:, 3:] == gt_class).any(dim=1).sum().item()
+            top3 += (top5_classes[:, :3] == gt_class).any(dim=1).sum().item()
+            top5 += (top5_classes[:, :] == gt_class).any(dim=1).sum().item()
 
     return conf / dataset_size, top1 / dataset_size, top3 / dataset_size, top5 / dataset_size
 
